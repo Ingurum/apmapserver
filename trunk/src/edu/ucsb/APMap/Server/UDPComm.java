3fda8f15fe536c6f5c8bd1c9390309098beca09e
mjeleva@gmail.com <mjeleva@gmail.com@fb85feab-76cf-2e84-5c10-f205607651df>
2011-02-10 18:53:09 +0000

    TraceParser implemented.

--- trunk/src/edu/ucsb/APMap/Server/UDPComm.java
+++ trunk/src/edu/ucsb/APMap/Server/UDPComm.java
@@ -3,13 +3,14 @@ import java.net.*;
 
 class UDPComm
 {
-	public static final String SERVERIP = "128.111.52.236";
-	public static final int SERVERPORT = 1234;
+	//public static final String SERVERIP = "128.111.52.236";
+	public static final String SERVERIP = "127.0.0.1";
+	public static final int SERVERPORT = 0;
 	
 	public static void main(String args[]) throws Exception
       {
 			InetAddress serverAddr = InetAddress.getByName(SERVERIP);
-         DatagramSocket serverSocket = new DatagramSocket(SERVERPORT, serverAddr);
+			DatagramSocket serverSocket = new DatagramSocket(SERVERPORT, serverAddr);
             byte[] receiveData = new byte[1024];
             while(true)
                {
