0bdf98b4b2621624e89ec71453b6621594228be9
mjeleva@gmail.com <mjeleva@gmail.com@fb85feab-76cf-2e84-5c10-f205607651df>
2011-02-19 02:26:17 +0000

    RSSI per location file added

---
+++ trunk/src/edu/ucsb/APMap/Server/LocationLevel.java
@@ -0,0 +1,46 @@
+package edu.ucsb.APMap.Server;
+
+public class LocationLevel {
+	
+	double longtitude;
+	double latitude;
+	int level;
+	
+	public LocationLevel(double longtitude, double latitude, int level) {
+		super();
+		this.longtitude = longtitude;
+		this.latitude = latitude;
+		this.level = level;
+	}
+
+	public double getLongtitude() {
+		return longtitude;
+	}
+
+	public void setLongtitude(double longtitude) {
+		this.longtitude = longtitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	
+	@Override
+	public String toString() {
+		return "Location [longtitude=" + longtitude + 
+			", latitude=" + latitude + ", level=" + level + "]";
+	}
+	
+}
