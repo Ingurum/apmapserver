8263c83218c89b8d2402c5b301b56e015b9e0529
blackice.wei@gmail.com <blackice.wei@gmail.com@fb85feab-76cf-2e84-5c10-f205607651df>
2011-02-28 07:17:07 +0000

    send file done, ap lookup according to current location done

--- trunk/src/edu/ucsb/APMap/util/DBOp.java
+++ trunk/src/edu/ucsb/APMap/util/DBOp.java
@@ -78,4 +78,44 @@ public class DBOp {
 		return apInfos;
 	}
 
+	
+	public String getAPInfos(double latitudeDouble, double longitudeDouble, double regionKilometer) {
+		String apInfos = "";
+		double latitudeDiff = regionKilometer / (40000.0 / 360);
+		double earth_radius = 6378.137;
+		double a = latitudeDouble * Math.PI / 180.0;
+		double b = 2 * Math.asin(Math.sqrt(Math.sin(regionKilometer / 2.0 / earth_radius) * Math.sin(regionKilometer / 2.0 / earth_radius) / Math.cos(a) / Math.cos(a)));
+		double longitudeDiff = Math.abs((longitudeDouble * Math.PI / 180.0 + b) * 180.0 / Math.PI - longitudeDouble);
+		
+		try {
+			Statement stmt = mysqlCon.createStatement();
+			String sqlStmt = "select * from aplocation where latitude >= " + (latitudeDouble - latitudeDiff)
+							 + " and latitude <= " + (latitudeDouble + latitudeDiff)
+							 + " and longtitude >= " + (longitudeDouble - longitudeDiff)
+							 + " and longtitude <= " + (longitudeDouble + longitudeDiff);
+			System.out.println("sqlStmt = " + sqlStmt);
+			System.out.println("longitudeDouble = " + longitudeDouble + ", longitudeDiff = " + longitudeDiff);
+			ResultSet rs = stmt.executeQuery(sqlStmt);
+			
+			while (rs.next()) {
+				String bssid = rs.getString("bssid");
+				String ssid = rs.getString("ssid");
+				String capabilities = rs.getString("capabilities");
+				String frequency = rs.getString("frequency");
+				String longtitude = rs.getString("longtitude");
+				String latitude = rs.getString("latitude");
+				
+				apInfos += bssid + ", " + ssid + ", " + capabilities + ", " + frequency + ", " 
+						+ longtitude + ", " + latitude + ";";
+				
+			}
+			
+			
+			System.out.println("done");
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+		}
+		System.out.println("apInfos = " + apInfos);
+		return apInfos;
+	}
 }
